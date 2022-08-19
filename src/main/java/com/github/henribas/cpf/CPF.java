d38207c3417d1232d8cdaa0d9cff0feb9e4361e8
henribas <henribas@gmail.com>
2022-08-19 08:05:47 -0300

    Formatação.

--- src/main/java/com/github/henribas/cpf/CPF.java
+++ src/main/java/com/github/henribas/cpf/CPF.java
@@ -3,14 +3,14 @@ package com.github.henribas.cpf;
 public interface CPF {
 
     public String numero();
-    
+
     public static String formatar(final String numero) {
         return CPFDidatico.formatar(numero);
     }
 
     public static String removerFormatacao(final String numero) {
-	    return CPFDidatico.removerFormatacao(numero);
-	}
+        return CPFDidatico.removerFormatacao(numero);
+    }
 
     public static boolean valido(final String numero) {
         return CPFDidatico.valido(numero);
@@ -31,5 +31,5 @@ public interface CPF {
     public static CPF de(final CPF cpf) {
         return CPFDidatico.de(cpf);
     }
-    
+
 }
--- src/main/java/com/github/henribas/cpf/CPFDidatico.java
+++ src/main/java/com/github/henribas/cpf/CPFDidatico.java
@@ -3,30 +3,30 @@ package com.github.henribas.cpf;
 import org.apache.commons.lang3.StringUtils;
 
 final class CPFDidatico implements CPF {
-    
-	private static final String CPF_SEM_MASCARA = "^\\d{11}$";
-	private static final String CPF_COM_MASCARA = "^\\d{3}.\\d{3}.\\d{3}-\\d{2}$";
 
-	private final String numero;
+    private static final String CPF_SEM_MASCARA = "^\\d{11}$";
+    private static final String CPF_COM_MASCARA = "^\\d{3}.\\d{3}.\\d{3}-\\d{2}$";
+
+    private final String numero;
 
     private CPFDidatico(final String numero) {
         validar(numero);
-		this.numero = removerFormatacao(numero);
-	}
+        this.numero = removerFormatacao(numero);
+    }
 
     private CPFDidatico(final String numero, final boolean validar) {
         if (validar) {
             validar(numero);
         }
-		this.numero = removerFormatacao(numero);
-	}
+        this.numero = removerFormatacao(numero);
+    }
 
     private CPFDidatico(final CPF cpf) {
         if (!valido(cpf)) {
             throw new IllegalArgumentException("Informe um CPF válido.");
         }
-		this.numero = cpf.numero();
-	}
+        this.numero = cpf.numero();
+    }
 
     private void validar(final String numero) {
         if (!valido(numero)) {
@@ -34,31 +34,31 @@ final class CPFDidatico implements CPF {
         }
     }
 
-	static CPF de(final String numero) {
-		return new CPFDidatico(numero);
-	}
+    static CPF de(final String numero) {
+        return new CPFDidatico(numero);
+    }
 
     static CPF de(final String numero, final boolean validar) {
-		return new CPFDidatico(numero, validar);
-	}
+        return new CPFDidatico(numero, validar);
+    }
 
     static CPF de(final CPF cpf) {
-		return new CPFDidatico(cpf);
-	}
+        return new CPFDidatico(cpf);
+    }
 
     static String formatar(final String numero) {
-		String numeroSemFormatacao = CPF.removerFormatacao(numero);
+        String numeroSemFormatacao = CPF.removerFormatacao(numero);
 
         return 
-            numeroSemFormatacao.substring(0,3) + "." + 
-            numeroSemFormatacao.substring(3,6) + "." + 
-            numeroSemFormatacao.substring(6,9) + "-" + 
-            numeroSemFormatacao.substring(9,11);
+            numeroSemFormatacao.substring(0, 3) + "." +
+            numeroSemFormatacao.substring(3, 6) + "." +
+            numeroSemFormatacao.substring(6, 9) + "-" +
+            numeroSemFormatacao.substring(9, 11);
     }
 
     static String removerFormatacao(final String numero) {
-		return numero.replaceAll("\\D", "");
-	}
+        return numero.replaceAll("\\D", "");
+    }
 
     static boolean valido(final String numero) {
         if (!informouNumero(numero)) {
@@ -68,12 +68,12 @@ final class CPFDidatico implements CPF {
         if (!estaNoPadrao(numero)) {
             return false;
         }
-		
+
         if (numeroInvalido(numero)) {
             return false;
         }
 
-		return osDigitosVerificadoresSaoIguais(numeros(numero));
+        return osDigitosVerificadoresSaoIguais(numeros(numero));
     }
 
     private static boolean informouNumero(final String numero) {
@@ -87,36 +87,36 @@ final class CPFDidatico implements CPF {
     private static boolean numeroInvalido(final String numero) {
         String numeroSemFormatacao = numero.replaceAll("\\D", "");
 
-        if (numeroSemFormatacao.equals("00000000000") || 
+        if (numeroSemFormatacao.equals("00000000000") ||
             numeroSemFormatacao.equals("11111111111") ||
-			numeroSemFormatacao.equals("22222222222") || 
+            numeroSemFormatacao.equals("22222222222") ||
             numeroSemFormatacao.equals("33333333333") ||
-			numeroSemFormatacao.equals("44444444444") || 
+            numeroSemFormatacao.equals("44444444444") ||
             numeroSemFormatacao.equals("55555555555") ||
-			numeroSemFormatacao.equals("66666666666") || 
+            numeroSemFormatacao.equals("66666666666") ||
             numeroSemFormatacao.equals("77777777777") ||
-			numeroSemFormatacao.equals("88888888888") || 
+            numeroSemFormatacao.equals("88888888888") ||
             numeroSemFormatacao.equals("99999999999")) {
 
-			return(true);
-		}
+            return (true);
+        }
 
         return false;
     }
 
-	private static int[] numeros(final String numero) {
-		String numeroSemFormatacao = numero.replaceAll("\\D", "");
+    private static int[] numeros(final String numero) {
+        String numeroSemFormatacao = numero.replaceAll("\\D", "");
         String[] numerosString = numeroSemFormatacao.split("");
-		int[] numeros = new int[11];
+        int[] numeros = new int[11];
 
-		for (int i = 0; i < numerosString.length; i++) {
-			numeros[i] = Integer.valueOf(numerosString[i]);
-		}
-		return numeros;
-	}
+        for (int i = 0; i < numerosString.length; i++) {
+            numeros[i] = Integer.valueOf(numerosString[i]);
+        }
+        return numeros;
+    }
 
-	private static int calcularPrimeiroDigitoVerificador(final int[] numeros) {
-        int[] multiplicadoresCPFSemDigitoVerificador = {10, 9, 8, 7, 6, 5, 4, 3, 2};
+    private static int calcularPrimeiroDigitoVerificador(final int[] numeros) {
+        int[] multiplicadoresCPFSemDigitoVerificador = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
         int[] resultadoMultiplicacaoCPFSemDigitoVerificador = new int[9];
 
         for (int i = 0; i < 9; i++) {
@@ -129,16 +129,17 @@ final class CPFDidatico implements CPF {
         }
 
         int resto = somaMultiplicacao % 11;
-        
-		return 11 - resto;
-	}
 
-	private static int calcularSegundoDigitoVerificador(final int[] numeros) {
-        int[] multiplicadoresCPFComUmDigitoVerificador = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
+        return 11 - resto;
+    }
+
+    private static int calcularSegundoDigitoVerificador(final int[] numeros) {
+        int[] multiplicadoresCPFComUmDigitoVerificador = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
         int[] resultadoMultiplicacaoCPFComUmDigitoVerificador = new int[10];
 
         for (int i = 0; i < 10; i++) {
-            resultadoMultiplicacaoCPFComUmDigitoVerificador[i] = numeros[i] * multiplicadoresCPFComUmDigitoVerificador[i];
+            resultadoMultiplicacaoCPFComUmDigitoVerificador[i] = numeros[i]
+                    * multiplicadoresCPFComUmDigitoVerificador[i];
         }
 
         int somaMultiplicacao = 0;
@@ -147,32 +148,31 @@ final class CPFDidatico implements CPF {
         }
 
         int resto = somaMultiplicacao % 11;
-        
-		return 11 - resto;
-	}
 
-	private static boolean osDigitosVerificadoresSaoIguais(final int[] numeros) {
+        return 11 - resto;
+    }
+
+    private static boolean osDigitosVerificadoresSaoIguais(final int[] numeros) {
         int primeiroDigitoVerificador = numeros[9];
         int segundoDigitoVerificador = numeros[10];
 
-		return 
-            (primeiroDigitoVerificador == calcularPrimeiroDigitoVerificador(numeros)) && 
-            (segundoDigitoVerificador == calcularSegundoDigitoVerificador(numeros));
-	}
+        return (primeiroDigitoVerificador == calcularPrimeiroDigitoVerificador(numeros)) &&
+               (segundoDigitoVerificador == calcularSegundoDigitoVerificador(numeros));
+    }
 
     static boolean valido(final CPF cpf) {
-		if (cpf == null) {
+        if (cpf == null) {
             return false;
         }
         return valido(cpf.numero());
     }
 
-	@Override
-	public String numero() {
-		return numero;
-	}
+    @Override
+    public String numero() {
+        return numero;
+    }
 
-	@Override
+    @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
@@ -198,8 +198,8 @@ final class CPFDidatico implements CPF {
     }
 
     @Override
-	public String toString() {
-		return "CPFDidatico [numero=" + numero + "]";
-	}
+    public String toString() {
+        return "CPFDidatico [numero=" + numero + "]";
+    }
 
 }
